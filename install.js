9be374303c5506a283836c0f733278fa2d41d20e
unknown <tom.chiu@hk.nx2mo.com>
2014-08-25 19:44:47 +0800

    basedir

---
+++ install.js
@@ -0,0 +1,11 @@
+﻿var err = initInstall("Babyish spellchecker", "bb@mimosapudica.org", "0.2.42.20131022");
+if (err != SUCCESS)
+    cancelInstall();
+
+var fProgram = getFolder("Program");
+err = addDirectory("", "bb@mimosapudica.org",
+		   "dictionaries", fProgram, "dictionaries", true);
+if (err != SUCCESS)
+    cancelInstall();
+
+performInstall();
