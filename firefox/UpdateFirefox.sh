6cce58fea91e6f390bcca0ef4319a7ca846b66b4
Tom Chiu @ nxTomo <tomchiukc@gmail.com>
2014-12-01 18:45:01 +0800

    Fayls editen gut 2014-12-01 à offis

--- firefox/UpdateFirefox.sh
+++ firefox/UpdateFirefox.sh
@@ -1,9 +1,11 @@
 #/bin/bash
-time -o ProcessRunTime-`date -I`.txt
+cd $(dirname "$0")
+time >> ProcessRunTime-`date -I`.txt
 git mv `ls *.xpi` BBSpelcek.zip
 git commit -m "Fayls editen gut `date -I` à offis"
 git mv BBSpelcek.zip BBSpelcek-`date -I`.xpi
 git add `ls *.xpi`
 git commit -m "Ripo ùpdeiten gut `date -I`"
 git push
-time
+time >> ProcessRunTime-`date -I`.txt
+
